0119058df4a036e815f117f8c7226ee6dba42207
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-01-04 15:09:35 +0900

    ・退勤時間の算出・設定情報の切り出し・デバッグログ設定

--- trInput.js
+++ trInput.js
@@ -1,33 +1,123 @@
-var casper = require("casper").create();
+var settings = {
+	startTime   : '09:00', // 始業時間
+	emailAddress: '***.***@ever-rise.co.jp',
+	password    : '********'
+};
 
-casper.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0");
+var time = new Date();
+var hour = time.getHours();
+var minute = time.getMinutes();
 
-casper.start();
-casper.open("http://tr.ever-rise.co.jp/dailyReportEdit/");
+/**
+ * ゼロパディング
+ *
+ * @param {number} number 取得した時間or分
+ * @return {number} 2桁にした数値
+ */
+function toDoubleDigits(number) {
+	// Number -> String
+    number += "";
+    if (number.length === 1) {
+      number = "0" + number;
+    }
+    return number;     
+}
 
-casper.then(function() {
-	 this.evaluate(function() {
-	    document.getElementsByName("emailAddress")[0].value = "daichi.ito@ever-rise.co.jp";
-	    document.getElementsByName("password")[0].value = "everrise";
-	    document.getElementsByName("doLogin")[0].click();
-	 }, true);
+/**
+ * 勤怠入力時間を算出
+ * 
+ * @param {number} hour 時間
+ * @param {number} minute 分
+ * @return {string} コロン繋ぎにした退勤時間
+ */
+function checkTime(hour, minute) {
+    hour = toDoubleDigits(hour);
+    
+	if (minute == 0) {
+		// 00
+        minute = toDoubleDigits(minute);
+	} else if (minute < 15) {
+		// 01 ~ 14
+        minute = '00';
+	} else if (minute < 30) {
+		// 15 ~ 29
+        minute = '15';
+	} else if (minute < 45) {
+		// 30 ~ 44
+        minute = '30';   
+	} else if (minute <= 59) {
+		// 45 ~ 59
+        minute = '45';
+    }
+
+ 	return hour + ":" + minute;
+}
+
+var endTime = checkTime(hour, minute);
+
+var workingHours = {
+	start: settings.startTime,
+	end  : endTime
+};
+
+var casper = require("casper").create({
+    verbose : true,
+    logLevel: "debug"
 });
 
+casper.start();
+
+// デバッグログ設定
+casper.on('page.resource.requested', function(requestData, request) {
+    require('utils').dump(requestData);
+});
+casper.on('remote.alert', function(msg) {
+    this.echo("REMOTE-ALERT: " + msg);
+});
+casper.on('remote.message', function(msg) {
+    this.echo("REMOTE-MESSAGE: " + msg);
+});
+casper.on("page.error", function(msg, trace) {
+    this.echo("REMOTE-ERROR: " + msg);
+    this.echo("TRACE:");
+    for (var i = 0; i < trace.length; i++) {
+        var trec = trace[i];
+        this.echo("file: " + trec.file + " , line: " + trec.line + " , function: " + trec.function);
+    }
+});
+
+casper.open("http://tr.ever-rise.co.jp/dailyReportEdit/");
+
+/**
+ * ログイン
+ * 
+ * @param {Object} settings 設定情報
+ */
 casper.then(function() {
-	this.evaluate(function() {
-		// TODO トークン取得する
-		
-	    document.getElementsByName("str_start_time")[0].value = "10:00";
-	    document.getElementsByName("str_end_time")[0].value = "19:00";
+	this.evaluate(function(settings) {
+	    document.getElementsByName("emailAddress")[0].value = settings.emailAddress;
+	    document.getElementsByName("password")[0].value = settings.password;
+	    document.getElementsByName("doLogin")[0].click();
+	}, settings);
+});
+
+/** 
+ * 勤怠入力
+ */
+casper.then(function() {
+	this.evaluate(function(workingHours) {
+	    document.getElementsByName("str_start_time")[0].value = workingHours.start;
+	    document.getElementsByName("str_end_time")[0].value = workingHours.end;
 	    document.getElementsByName("db_repose_time")[0].value = "1.00";
 	    document.getElementsByClassName("selectedValue")[0].click();
 	    document.getElementsByClassName("itm-7")[0].click();
 	    document.getElementsByClassName("btn-open")[0].click();
 	    document.getElementsByName("reportadd")[0].click();
-    }, true);
+    }, workingHours);
 });
 
 casper.then(function() {
-    this.capture('test-finished.png');
+    this.capture('finished.png');
 });
+
 casper.run();
